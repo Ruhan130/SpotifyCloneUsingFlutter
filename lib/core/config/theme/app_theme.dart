caeaa0faebcb5f554f43b2a5794f00b8b00c2e09
Ruhan130 <ruhank170@gmail.com>
2024-12-16 12:02:17 +0500

    App_theme_dart defined

---
+++ lib/core/config/theme/app_theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:project/core/config/theme/app_color.dart';
+
+class Apptheme {
+  static final lightTheme = ThemeData(
+    primaryColor: AppColor.primary,
+    brightness: Brightness.light,
+    scaffoldBackgroundColor: AppColor.lightBackground,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+      backgroundColor: AppColor.primary,
+      textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+    )),
+  );
+
+
+  static final darkTheme = ThemeData(
+    primaryColor: AppColor.primary,
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: AppColor.darkBackground,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+      backgroundColor: AppColor.primary,
+      textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+    )),
+  );
+}
