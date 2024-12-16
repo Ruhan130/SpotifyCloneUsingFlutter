d7b7399355b3be7c7fd4ba70e389e3df5f44ac84
Ruhan130 <ruhank170@gmail.com>
2024-12-16 15:52:14 +0500

    Chose Mode Work Done

---
+++ lib/common/customColumnChoseMode.dart
@@ -0,0 +1,37 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:project/common/customTextWiget.dart';
+import 'package:project/core/config/assets/app_vectors.dart';
+import 'package:project/core/config/theme/app_color.dart';
+
+class Customcolumnchosemode extends StatelessWidget {
+  final String text ;
+  final String picture;
+  const Customcolumnchosemode({super.key, required this.text,required this.picture});
+  
+  // final String text ;
+
+  @override
+  Widget build(BuildContext context) {
+    return   Column(
+                      children: [
+                        ClipOval(
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                            child: Container(
+                              height: 50,
+                              width: 50,
+                              decoration:  BoxDecoration(
+                                color: const Color(0xff30393C).withOpacity(0.5),
+                              ),
+                              child: SvgPicture.asset(picture ,fit: BoxFit.none,),
+                            ),
+                          ),
+                        ),
+                         CustomTextwiget(text: text, color: AppColor.textColorWhite, fontWeight: FontWeight.w500,)
+                      ],
+                    );
+  }
+}
--- lib/core/config/assets/app_image.dart
+++ lib/core/config/assets/app_image.dart
@@ -2,4 +2,5 @@ class AppImage{
   static const String basePath = 'assets/images/' ;
 
   static const String introBG = '${basePath}intro_bg.png';
+  static const String choseModepicture = '${basePath}choose_mode_bg.png';
 }
