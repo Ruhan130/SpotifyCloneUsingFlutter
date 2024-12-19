7af5cac32fa7dd4b05028b7a6eaa4217e6408f9f
Ruhan130 <ruhank170@gmail.com>
2024-12-19 11:02:05 +0500

    Some changes inBAsic App bar

--- lib/presentation/Home/pages/HomeScreen.dart
+++ lib/presentation/Home/pages/HomeScreen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:project/common/widgets/Basic_appbar.dart';
 
 class RootPage extends StatelessWidget {
   const RootPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: BasicAppbar(),
+    );
   }
 }
