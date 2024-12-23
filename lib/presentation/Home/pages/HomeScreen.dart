71b2d4dc7358ddc0421bc40cf4da328aa4c99008
Ruhan130 <ruhank170@gmail.com>
2024-12-23 16:06:12 +0500

    Adjust some size

--- lib/presentation/Home/pages/HomeScreen.dart
+++ lib/presentation/Home/pages/HomeScreen.dart
@@ -7,7 +7,6 @@ import 'package:project/core/config/assets/app_image.dart';
 import 'package:project/core/config/assets/app_vectors.dart';
 import 'package:project/core/config/theme/app_color.dart';
 
-
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -17,14 +16,21 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
-      
   late TabController _tabController;
 
   final List<Map<String, String>> songs = [
-    {"name": "Lovely", "title": "Billie Bilish", "image": AppImage.bili_eilish2 },
+    {
+      "name": "Lovely",
+      "title": "Billie Bilish",
+      "image": AppImage.bili_eilish2
+    },
     {"name": "One Dance", "title": "Drake", "image": AppImage.Drake1},
     {"name": "Perfect", "title": "Ed Sheeran", "image": AppImage.ed_sheeran},
-    {"name": "Say Yes to heaven", "title": "Lana Del Rey", "image": AppImage.lana_del_rey1},
+    {
+      "name": "To heaven",
+      "title": "Lana Del Rey",
+      "image": AppImage.lana_del_rey1
+    },
   ];
 
   @override
@@ -54,7 +60,9 @@ class _HomePageState extends State<HomePage>
             ),
             const SizedBox(height: 10),
             _tabs(context),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
             // Add a specific height or other constraints to the ListView
             SizedBox(
               height: 250, // Explicit height
@@ -122,61 +130,56 @@ class _HomePageState extends State<HomePage>
       shrinkWrap: true, // Ensures ListView doesn't take more space than needed
       itemCount: songs.length,
       itemBuilder: (context, index) {
-        return 
-         
-        SizedBox(
-            width: 160,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 15),
-              child: Column(
-                
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
+        return SizedBox(
+          width: 160,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 200,
+                  width: 140,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(songs[index]['image'] ?? ''),
+                    ),
+                  ),
+                  child: Align(
+                    alignment: Alignment.bottomRight,
                     child: Container(
-                      height: 180,
-                      width: 140,
-                      decoration:  BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        image: DecorationImage(
-                          fit: BoxFit.fill,
-                          image: AssetImage(songs[index]['image'] ?? ''),
-                        ),
-                      ),
-                      child: Align(
-                        alignment: Alignment.bottomRight,
-                        child: Container(
-                          height: 40,
-                          width: 40,
-                          transform: Matrix4.translationValues(10, 10, 0),
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: context.isDarkMode
-                                  ? AppColor.darkGrey
-                                  : const Color(0xffE6E6E6)),
-                          child: Icon(
-                            Icons.play_arrow_rounded,
-                            color: context.isDarkMode ? Colors.white : Colors.black,
-                          ),
-                        ),
+                      height: 40,
+                      width: 40,
+                      transform: Matrix4.translationValues(10, 10, 0),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: context.isDarkMode
+                              ? AppColor.darkGrey
+                              : const Color(0xffE6E6E6)),
+                      child: Icon(
+                        Icons.play_arrow_rounded,
+                        color: context.isDarkMode ? Colors.white : Colors.black,
                       ),
                     ),
                   ),
-                  CustomTextwiget(
-                    text: songs[index]['name'] ?? '',
-                    color: context.isDarkMode ? Colors.black : Colors.white,fontWeight: FontWeight.w500,
-                    textFontsize: 18,
-                  ),
-                  CustomTextwiget(
-                    text: songs[index]['title'] ?? '',
-                    color: context.isDarkMode ? Colors.black : Colors.white,
-                    textFontsize: 12,
-                  ),
-                ],
-              ),
+                ),
+                CustomTextwiget(
+                  text: songs[index]['name'] ?? '',
+                  color: context.isDarkMode ? Colors.black : Colors.white,
+                  fontWeight: FontWeight.w500,
+                  textFontsize: 18,
+                ),
+                CustomTextwiget(
+                  text: songs[index]['title'] ?? '',
+                  color: context.isDarkMode ? Colors.black : Colors.white,
+                  textFontsize: 12,
+                ),
+              ],
             ),
-          );
-  
+          ),
+        );
       },
       separatorBuilder: (context, index) => const SizedBox(
         width: 0,
