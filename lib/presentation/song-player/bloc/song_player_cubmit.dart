f552fe8a771894a63b7c92af7c6060b3c2595f78
Ruhan130 <ruhank170@gmail.com>
2024-12-24 12:32:26 +0500

    Bloc cubit work done

--- lib/presentation/song-player/bloc/song_player_cubmit.dart
+++ lib/presentation/song-player/bloc/song_player_cubmit.dart
@@ -1,11 +1,52 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:project/core/config/assets/app_music.dart';
 import 'package:project/presentation/song-player/bloc/song_player_state.dart';
 
-class SongPlayerCubmit extends Cubit<SongPlayerState>{
-  SongPlayerCubmit() : super (SongPlayerLoading());
-
+class SongPlayerCubmit extends Cubit<SongPlayerState> {
   AudioPlayer audioPlayer = AudioPlayer();
-    
 
+  Duration songDuration = Duration.zero;
+  Duration songPosition = Duration.zero;
+
+
+  SongPlayerCubmit() : super(SongPlayerLoading()){
+    audioPlayer.durationStream.listen((position){
+      songPosition = position!;
+      updateSongPlayer();
+    });
+
+    audioPlayer.durationStream.listen((duration){
+      songDuration = duration!;
+    });
+  }
+  void updateSongPlayer(){
+    emit(
+      SongPlayerLoaded()
+    );
+  }
+
+  Future<void> loadSong(String url) async {
+    try {
+      await audioPlayer.setAsset(AppMusic.lovely_by_billi);
+      emit(SongPlayerLoaded());
+    } catch (e) {
+      emit(SongPlayerLoaded());
+    }
+  }
+
+  void PlayorPause() {
+    if (audioPlayer.playing) {
+      audioPlayer.stop();
+    } else {
+      audioPlayer.play();
+    }
+    emit(SongPlayerLoaded());
+  }
+
+  @override
+  Future <void> close(){
+    audioPlayer.dispose();
+    return super.close();
+  }
 }
