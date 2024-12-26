6b71a66ac15af45569af90970bd1a56b1a597b4d
Ruhan130 <ruhank170@gmail.com>
2024-12-26 10:27:23 +0500

    Start working on Add to favouirte

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:project/core/config/theme/app_theme.dart';
 import 'package:project/firebase_options.dart';
+import 'package:project/presentation/Home/pages/HomeScreen.dart';
 import 'package:project/presentation/chooseModePage/bloc/theme_cubit.dart';
 import 'package:project/presentation/splash/pages/splash.dart';
 import 'package:project/service_locator.dart';
@@ -40,7 +41,7 @@ class MyApp extends StatelessWidget {
           darkTheme: Apptheme.darkTheme,
           themeMode: mode,
           debugShowCheckedModeBanner: false,
-          home: const SplashPage(),
+          home: const HomePage(),
         ),
       ),
     );
